8fb31679925906a9622d89d5119474fd070adfd1
TaufeeqR <taurajap@gmail.com>
2019-02-07 11:37:41 +0200

    Created php files.

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Hotel</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Hotel</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/index.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+
+    <div class="nav-bar">
+        <a href="">Home</a>
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+        
+    <div class="header-container">
+        <div class="header">
+            <h1>Book A Hotel</h1>
+        </div>
+    </div>
+
+    </body>
 </html>
