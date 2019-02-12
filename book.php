9cc98683abcc2a33262c49560b9342be3264c319
TaufeeqR <taurajap@gmail.com>
2019-02-12 14:49:58 +0200

    Updated Book.php

--- book.php
+++ book.php
@@ -1,21 +1,69 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Booking</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Booking</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/book.css"/>
+        <script src="main.js"></script>
+    </head>
+    <body>
 
-<div class="header">
-    <h1 class="heading">
-        Book Your Ideal Hotel
-    </h1>
-</div>
+    <!-- Navbar for the bookings page below -->
 
+        <div class="navbar">
+            <a href="book.php">Book Now!</a>
+            <a href="register.php">Register</a>
+            <a href="login.php" >Sign In</a>
+            <a href="index.php" class="hero">
+                <img src="" alt="logo">
+            </a>
+        </div>
 
-</body>
+        <!-- Main header for the page -->
+
+        <div class="header">
+            <h1 class="heading">
+                Book Your Ideal Hotel
+            </h1>
+        </div>
+
+        <!-- Main container with booking content below -->
+
+        <div class="grid-container">
+            <div class="grid">
+                <div class="hotel-drpdwn">
+                    <h2>Select Your Hotel</h2>
+                    <select name="hotel-list">
+                        <option name="hotel-one" value="">Crystal hotel and Spa</option>
+                        <option name="hotel-two" value="">Cape Lodge Hotel</option>
+                        <option name="hotel-three" value="">Hotel Cezar</option>
+                        <option name="hotel-four" value="">Presidente Hotel</option>
+                    </select>
+                </div>
+
+                <div class="check-in">
+                    <h2>Check In</h2>
+                    <select name="hotels">
+                        <option value=""></option>
+                    </select>
+                </div>
+
+                <div class="check-out">
+                    <h2>Check Out</h2>
+                    <select name="hotels">
+                        <option value=""></option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="grid">
+                <div class="hero-image">
+                    <img src="img/cDr2JGj.jpg" alt="logo">
+                </div>
+            </div>
+        </div>
+
+    </body>
 </html>
--- index.php
+++ index.php
@@ -5,27 +5,31 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Hotel</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="../css/index.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="../css/index.css"/>
         <script src="main.js"></script>
     </head>
     <body>
 
     <div class="navbar">
-        <a href="">Book Now!</a>
+        <a href="book.php">Book Now!</a>
         <a href="register.php">Register</a>
         <a href="login.php" >Sign In</a>
-        <a href="" class="hero">
-            <img src="img/LogoMakr_1DsaXG.png" alt="logo">
+        <a href="index.php" class="hero">
+            <img src="" alt="logo">
         </a>
     </div>
 
 <!-- Grid containing the different hotels and their features -->
 
+    <div class="heading">
+        <h2>Book A Hotel</h2>
+    </div>
+
     <div class="grid-container">
 
         <div class="grid">
             <div class="hotel-one">
-                <img src="img/hotels/hotel-1.jpg" alt="image">
+                <img src="img/hotels/hotel-1.jpg" alt="image" class="hotel-img">
                 <h2>CRYSTAL HOTEL AND SPA</h2>
                 <p>Nestled at the foot of Table Mountain, Crystal hotel and Spa is located within walking distance of the variety of trendy shops and restaurants on the active Cape Town Streets. And 10 minutes away are attractions such as V&A Waterfront, Camps Bay and Clifton Beaches. Some rooms boast views of Table Mountain. The hotel connects 5 beautiful heritage properties via a central reception and secluded courtyard. Guests can relax on the sun deck, and enjoy a splash in the plunge pools or the hot tub.</p>
 
@@ -50,11 +54,10 @@
                     </ul>
             </div>
         </div>
-        
 
         <div class="grid">
             <div class="hotel-two">
-                <img src="img/hotels/hotel-2.jpg" alt="image">
+                <img src="img/hotels/hotel-2.jpg" alt="image" class="hotel-img">
                 <h2>CAPE LODGE HOTEL</h2>
                 <p>The 5-star luxury hotel celebrates the beauty of Cape Town with panoramic views. It has been nominated for the 2016 World Travel Awards. The Capers Spa pampers guests. Cape Lodge Hotel boasts a 24-hour fitness centre, a private movie theatre with 29 luxurious seats, a swimming pool. The Sinatra’s restaurant serves European continental cuisine in an elegant setting inspired by Frank Sinatra. Or guests can order drinks while listening to pianists at the Paparazzi Bar & Lounge. Cape Lodge Hotel also offers a Rolls Royce for chauffeuring guests.</p>
 
@@ -82,7 +85,7 @@
 
         <div class="grid">
             <div class="hotel-three">
-                <img src="img/hotels/hotel-3.jpg" alt="image">
+                <img src="img/hotels/hotel-3.jpg" alt="image" class="hotel-img">
                 <h2>HOTEL CEZAR</h2>
                 <p>Hotel Cezar is based between the V&A Waterfront and one of the world heritage site in Cape Town, table Mountain. This luxury 5-star hotel boasts modern conference facilities. All rooms have contemporary designs, stylishly decorated with beautiful carpets and wooden furniture. The hotel features an enormous outdoor pool with lovely views of the Signal Hill. The well-equipped fitness room includes a steam room, and a sauna. Signal Hill Terrace offers delectable drinks such as tea, coffee, and fresh fruit juices. The South African National Gallery and Castle of Good Hope are 5-minutes away by car.</p>
 
@@ -108,9 +111,9 @@
 
         <div class="grid">
             <div class="hotel-four">
-                <img src="img/hotels/hotel-4.jpg" alt="image">
+                <img src="img/hotels/hotel-4.jpg" alt="image" class="hotel-img">
                 <h2>PRESIDENTE HOTEL</h2>
-                <p>Presidente Hotel is located within the V&A Waterfront, just a walking distance from the Victoria Wharf Shopping Centre. Guests can enjoy soothing treatments in the Signature Spa. Mountain or piazza views can be enjoyed from the rooms, all of which boast wonderful in-room amenities. Presidente Hotel offers on-site facilities such as a gym, conference rooms, and free Wi-Fi. Guests will also have access to secure parking, laundry services, and a 24-hour front desk. Artisanal-style dishes are served in the Ginja Restaurant, where guests can enjoy views of Table Mountain while dining at breakfast, lunch, and dinner. Just a 4 minute walk is the Two Oceans Aquarium and about 2 km from Cape Town International Convention Centre.</p>
+                <p>Presidente Hotel is located within the V&A Waterfront, just a walking distance from the Victoria Wharf Shopping Centre. Guests can enjoy soothing treatments in the Signature Spa. Mountain or piazza views can be enjoyed from the rooms, all of which boast wonderful in-room amenities. Guests will also have access to secure parking, laundry services, and a 24-hour front desk. Artisanal-style dishes are served in the Ginja Restaurant, where guests can enjoy views of Table Mountain while dining at breakfast, lunch, and dinner. Just a 4 minute walk is the Two Oceans Aquarium and about 2 km from Cape Town International Convention Centre.</p>
 
                 <h3>Top Amenities</h3>
                     <ul>
